ac39fc5341137b015b157047cb9faff4f358e07e
onlinecalculatorpro-astro <onlinecalculatorpro@gmail.com>
2025-09-07 16:55:19 +0530

    Update __init__.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,1 +1,18 @@
+"""
+Core research-grade astrological computation modules.
 
+This package contains all the research-grade modules with observatory-level
+precision and complete uncertainty propagation.
+"""
+
+from .timescales import TimeScales, UncertaintyBounds, build_timescales_research
+from .system_integration import ResearchGradeSystem, SystemConfig, run_full_system_integration
+
+__all__ = [
+    "TimeScales",
+    "UncertaintyBounds", 
+    "build_timescales_research",
+    "ResearchGradeSystem",
+    "SystemConfig",
+    "run_full_system_integration",
+]
